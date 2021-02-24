f582edd5ff090a21539880e891f18569e0c65f4f
José Miguel Rodrigues <josemiguel.rodrigues@farfetch.com>
2021-02-24 17:33:33 +0000

    readded zip check

--- docker/scripts/package_build.sh
+++ docker/scripts/package_build.sh
@@ -17,24 +17,28 @@ fi
 
 # For each .zip file on the source folder
 for file in $SOURCES_PATH/*.zip; do
-    export FILENAME=$(basename $file) # remove folder source prefix
-    echo "=> $FILENAME is .zip type, going to unzip."
-    export BASE_FILENAME=$(basename $FILENAME .zip) # remove .zip suffix
+    if file --mime-type "$file" | grep -q zip$; then
+        export FILENAME=$(basename $file) # remove folder source prefix
+        echo "=> $FILENAME is .zip type, going to unzip."
+        export BASE_FILENAME=$(basename $FILENAME .zip) # remove .zip suffix
 
-    # unzip the .zip to ./tmp folder
-    unzip -q $file -d $TMP_PATH/$BASE_FILENAME
+        # unzip the .zip to ./tmp folder
+        unzip -q $file -d $TMP_PATH/$BASE_FILENAME
 
-    # go to ./tmp/the_name_of_file
-    pushd $TMP_PATH/$BASE_FILENAME
-    if [[ -f "pom.xml" ]]; then
-        echo "=> Haves pom.xml to be compiled by maven"
-        mvn -B -nsu -V -DskipTests -DskipDocker install
+        # go to ./tmp/the_name_of_file
+        pushd $TMP_PATH/$BASE_FILENAME
+        if [[ -f "pom.xml" ]]; then
+            echo "=> Haves pom.xml to be compiled by maven"
+            mvn -B -nsu -V -DskipTests -DskipDocker install
+        else
+            echo "=> Does not have pom.xml no need to be compiled move to --> $PACKAGE_PATH"
+            # copy .zip from source folder => to package folder
+            cp $BASE_PATH/$SOURCES_PATH/$FILENAME $BASE_PATH/$PACKAGE_PATH
+        fi
+        popd
     else
-        echo "=> Does not have pom.xml no need to be compiled move to --> $PACKAGE_PATH"
-        # copy .zip from source folder => to package folder
-        cp $BASE_PATH/$SOURCES_PATH/$FILENAME $BASE_PATH/$PACKAGE_PATH
+        echo "=> $file != .zip type --> skiped..."
     fi
-    popd
 done
 
 rm -R $TMP_PATH/*
